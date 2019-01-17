001a6d50aae27f3152f531854e1b8f5d27506bb1
Jacob Gathof <gathofjd@rose-hulman.edu>
2019-01-16 19:05:13 -0500

    working on Database implementation

--- app/src/main/java/edu/rose_hulman/tee/social_litter/Database.kt
+++ app/src/main/java/edu/rose_hulman/tee/social_litter/Database.kt
@@ -1,4 +1,21 @@
 package edu.rose_hulman.tee.social_litter
 
+import com.google.firebase.firestore.FirebaseFirestore
+
 class Database {
+
+    private val databaseRef = FirebaseFirestore
+        .getInstance()
+        .collection(USERS)
+
+
+    fun populateTestData(){
+        databaseRef.add("")
+
+    }
+
+    companion object {
+        val USERS = "USERS"
+    }
+
 }
